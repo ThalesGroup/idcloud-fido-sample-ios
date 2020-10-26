17d70e88a09e9bc294033e019bfd5854ce09c23a
Mervyn Ong <mervyn.ong@thalesgroup.com>
2020-10-26 22:38:04 +0800

    remove code to make GettingStarted uncompilable

--- app/idcloudclientsample/idcloudclientsample/GettingStarted/idcloudclient/Authenticate.swift
+++ app/idcloudclientsample/idcloudclientsample/GettingStarted/idcloudclient/Authenticate.swift
@@ -29,28 +29,20 @@ class Authenticate : NSObject {
         // Required callbacks are essential to ensure a proper UX behaviour.
         // As a means of convenience, the IdCloud FIDO UI SDK provides a ClientConformer class which conforms to all necessary delegates of IdCloud FIDO SDK
         /* 1 */
-        let idcUiDelegates = IDCUiDelegates()
-        idcUiDelegates.commonUiDelegate = uiDelegates
-        idcUiDelegates.biometricUiDelegate = uiDelegates
-        idcUiDelegates.securePinPadUiDelegate = uiDelegates
+        
+        
         
         // Create an instance of the Fetch request.
         // Instances of requests should be held as an instance variable to ensure that completion callbacks will function as expected and to prevent unexpected behaviour.
         /* 2 */
-        request = idcloudclient.createFetchRequest(with: idcUiDelegates, progress: { (progress) in
-            // Refer to IDCProgress for corresponding callbacks which provide an update to the existing request execution.
-            progressClosure(progress)
-        }, completion: { (response, error) in
-            // Callback to the UI.
-            // These are executed on the Main thread.
-            completion(error as NSError?)
-        })
+        
+        
         
         // Execute the request.
         // Requests on IdClouf FIDO SDK are executed on the own unique threads.
         // Ensure that an Authentication scenario was previously invoked. Otherwise, an Error will be returned when no actionable events are present.
         /* 3 */
-        request.execute()
+        
+        
     }
-
 }
--- app/idcloudclientsample/idcloudclientsample/GettingStarted/idcloudclient/Enroll.swift
+++ app/idcloudclientsample/idcloudclientsample/GettingStarted/idcloudclient/Enroll.swift
@@ -30,34 +30,27 @@ class Enroll : NSObject {
         // Initialize an instance of IDCEnrollmentCredential from its corresponding Factory.
         // Instances of IDCEnrollmentCredential are initialized with a code retrieved from the Bank via a QR code (i.e. or other means) and is simply encoded as a UTF8 data.
         /* 1 */
-        let credentials = IDCEnrollmentCredentialsFactory.createEnrollmentCredentials(code.data(using: .utf8)!)
+        
+        
         
         // Set up an instance of IDCUiDelegates, an encapsulated class containing all necessary UI delegates required by IdCloud FIDO SDK.
         // Ensure that you conform to these corresponding delegates.
         // Required callbacks are essential to ensure a proper UX behaviour.
         // As a means of convenience, the IdCloud FIDO UI SDK provides a ClientConformer class which conforms to all necessary delegates of IdCloud FIDO SDK
         /* 2 */
-        let idcUiDelegates = IDCUiDelegates()
-        idcUiDelegates.commonUiDelegate = uiDelegates
-        idcUiDelegates.biometricUiDelegate = uiDelegates
-        idcUiDelegates.securePinPadUiDelegate = uiDelegates
+        
+        
         
         // Create an instance of the Enrollment request providing the required credentials.
         // Instances of requests should be held as an instance variable to ensure that completion callbacks will function as expected and to prevent unexpected behaviour.
         /* 3 */
-        request = idcloudclient.createEnrollRequest(with: credentials,
-                                                    uiDelegates: idcUiDelegates, progress: { (progress) in
-                                                        // Refer to IDCProgress for corresponding callbacks which provide an update to the existing request execution.
-                                                        progressClosure(progress)
-                                                    }, completion: { (response, error) in
-                                                        // Callback to the UI.
-                                                        // These are executed on the Main thread.
-                                                        completion(error as NSError?)
-                                                    })
+        
+        
         
         // Execute the request.
         // Requests on IdClouf FIDO SDK are executed on the own unique threads.
         /* 4 */
-        request.execute()
+        
+        
     }
 }
